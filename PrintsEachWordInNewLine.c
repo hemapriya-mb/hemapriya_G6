8fb0d4d40cc7bfd37a8812f22187ff2c1b57696d
hemapriya-mb <129997303+hemapriya-mb@users.noreply.github.com>
2023-04-18 22:38:21 +0530

    Create PrintsEachWordInNewLine.c

---
+++ PrintsEachWordInNewLine.c
@@ -0,0 +1,17 @@
+// program that takes a sentence as input and prints each word in the sentence on a new line. 
+#include <stdio.h>
+int main() {
+ char sentence[1000];
+ int i, j;
+ printf("Enter a sentence: ");
+ scanf("%[^\n]",sentence);
+ for (i = 0; sentence[i] != '\0'; i++) {
+   if (sentence[i] == ' ') {
+     printf("\n");
+   }
+   else {
+     printf("%c", sentence[i]);
+   }
+ }
+ return 0;
+}
