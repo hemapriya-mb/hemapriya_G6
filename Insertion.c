3aa845f3647a6a2ef412d4e0a243d5300ad26106
hemapriya-mb <129997303+hemapriya-mb@users.noreply.github.com>
2023-04-18 22:21:31 +0530

    Create Insertion.c

---
+++ Insertion.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node *left, *right;
+};
+
+struct node* insert(struct node *root, int value) {
+    if (root == NULL) {
+        struct node newnode = (struct node) malloc(sizeof(struct node));
+        newnode->data = value;
+        newnode->left = NULL;
+        newnode->right = NULL;
+        
+        return newnode;
+    }
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else {
+        root->right = insert(root->right, value);
+    }
+    
+    return root;
+}
+
+void print(struct node *root) {
+    if (root == NULL) {
+        return ;
+    }
+    print(root->left);
+    printf("%d ", root->data);
+    print(root->right);
+    
+    return ;
+}
+
+int main(){
+    struct node *root=NULL;
+    int num,data,value;
+    printf("Enter the number of elements: ");
+    scanf("%d",&num);
+    printf("Enter the elements: ");
+    for (int i=0;i<num;i++){
+        scanf("%d",&data);
+        root = insert(root,data);
+    }
+    print(root);
+    printf("\n");
+  
+return 0;
+}
