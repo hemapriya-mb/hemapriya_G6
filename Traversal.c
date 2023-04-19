5229863535d6e8fe361b7fe8d3238d84b5a0d05c
hemapriya-mb <129997303+hemapriya-mb@users.noreply.github.com>
2023-04-19 23:23:08 +0530

    Update Traversal.c

--- Traversal.c
+++ Traversal.c
@@ -92,7 +92,7 @@ void breadthFirstTraversal(struct node* root) {
 
 int main(){
     struct node *root=NULL;
-    int num,data,value;
+    int num,data;
     printf("Enter the number of elements: ");
     scanf("%d",&num);
     printf("Enter the elements: ");
