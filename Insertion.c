2edb75ffa9710785f5e89f29f1339555c0d85841
hemapriya-mb <129997303+hemapriya-mb@users.noreply.github.com>
2023-04-19 23:20:48 +0530

    Update Insertion.c

--- Insertion.c
+++ Insertion.c
@@ -37,7 +37,7 @@ void print(struct node *root) {
 
 int main(){
     struct node *root=NULL;
-    int num,data,value;
+    int num,data;
     printf("Enter the number of elements: ");
     scanf("%d",&num);
     printf("Enter the elements: ");
