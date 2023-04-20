da0b6f88ae4b1294411fff572c8cae105abf4a8a
hemapriya-mb <129997303+hemapriya-mb@users.noreply.github.com>
2023-04-20 18:51:41 +0530

    Update PrintEachIntegerOnNewLine.c

--- PrintEachIntegerOnNewLine.c
+++ PrintEachIntegerOnNewLine.c
@@ -1,24 +1,40 @@
 // program that takes a string of integers separated by commas as input and enclosed by braces and then prints each integer on a new line.
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     char input[100];
     printf("Enter a string of integers enclosed by braces: ");
     scanf("%[^\n]", input);
-    int i = 0;
-    while (input[i] != '{') {
-        i++;
+    if (input[0] != '{') {
+        printf("Error: Input string must start with '{'\n");
+        exit(1);
     }
-    i++; 
+    int i = 1;
     while (input[i] != '}') {
+        if (!isdigit(input[i]) && input[i] != ',') {
+            printf("Error: Invalid character '%c'\n", input[i]);
+            exit(1);
+        }
         int num = 0;
-        while (input[i] != ',' && input[i] != '}') {
+        while (isdigit(input[i])) {
             num = num * 10 + (input[i] - '0');
             i++;
         }
         printf("%d\n", num);
-        i++;
+        if (input[i] != ',' && input[i] != '}') {
+            printf("Error: Expected ',' or '}' after integer\n");
+            exit(1);
+        }
+        if (input[i] == ',') {
+            i++;
+        }
     }
-    
+    if (input[i] != '}') {
+        printf("Error: Input string must end with '}'\n");
+        exit(1);
+    }
+
     return 0;
 }
+
