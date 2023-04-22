b48359d6fa89c2616c8367f85d2c17b93ab4a7d3
hemapriya-mb <129997303+hemapriya-mb@users.noreply.github.com>
2023-04-23 00:11:58 +0530

    Update RemoveCharacters.c

--- RemoveCharacters.c
+++ RemoveCharacters.c
@@ -14,16 +14,15 @@ int main() {
   scanf("%s", string2);
 
   for (i = 0; i < strlen(string2); i++) {
-    for (j = 0; j < strlen(string1); j++) {
-      if (string2[i] == string1[j]) {
-        for (k = j; k < strlen(string1); k++) {
-          string1[k] = string1[k + 1];
-        }
-        j--;
+      for (j = 0; j < strlen(string1); j++) {
+           if (string2[i] == string1[j]) {
+                 for (k = j; k < strlen(string1); k++) {
+                    string1[k] = string1[k + 1];
+                 }
+                 j--;
+           }
       }
-    }
   }
-
   printf("Result: %s\n", string1);
 
   return 0;
