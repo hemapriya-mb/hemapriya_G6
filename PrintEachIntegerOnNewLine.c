f61a966a8707a6f35fae75bbbf42608b0e8af43a
hemapriya-mb <129997303+hemapriya-mb@users.noreply.github.com>
2023-04-23 00:09:14 +0530

    Update PrintEachIntegerOnNewLine.c

--- PrintEachIntegerOnNewLine.c
+++ PrintEachIntegerOnNewLine.c
@@ -6,34 +6,39 @@ int main() {
     char input[100];
     printf("Enter a string of integers enclosed by braces: ");
     scanf("%[^\n]", input);
+    
     if (input[0] != '{') {
         printf("Error: Input string must start with '{'\n");
         exit(1);
     }
+    
     int i = 1;
     while (input[i] != '}') {
-        if (!isdigit(input[i]) && input[i] != ',') {
+            if (!isdigit(input[i]) && input[i] != ',') {
             printf("Error: Invalid character '%c'\n", input[i]);
             exit(1);
-        }
-        int num = 0;
-        while (isdigit(input[i])) {
+            }
+        
+            int num = 0;
+            while (isdigit(input[i])) {
             num = num * 10 + (input[i] - '0');
             i++;
-        }
-        printf("%d\n", num);
-        if (input[i] != ',' && input[i] != '}') {
+            }
+            printf("%d\n", num);
+        
+            if (input[i] != ',' && input[i] != '}') {
             printf("Error: Expected ',' or '}' after integer\n");
             exit(1);
-        }
-        if (input[i] == ',') {
+            }
+        
+            if (input[i] == ',') {
             i++;
+            }
         }
-    }
     if (input[i] != '}') {
         printf("Error: Input string must end with '}'\n");
         exit(1);
-    }
+     }
 
     return 0;
 }
