8d043f90c4f80d5db4dbf62926e26bf9fddc805c
hemapriya-mb <129997303+hemapriya-mb@users.noreply.github.com>
2023-04-23 21:44:21 +0530

    Update WordsInSentence.c

--- WordsInSentence.c
+++ WordsInSentence.c
@@ -4,12 +4,18 @@
 
 int main() {
     char sentence[1000];
-    int words = 1;  
+    int words = 0;  
     printf("Enter a sentence: ");
     scanf("%[^\n]", sentence);
     for (int i = 0; i < strlen(sentence); i++) {
         if (sentence[i] == ' ' || sentence[i] == '\n') {
-            words++;
+            while (sentence[i+1] == ' ' || sentence[i+1] == '\n') {
+                i++;
+            }
+        } else {
+            if (i == 0 || sentence[i-1] == ' ' || sentence[i-1] == '\n') {
+                words++;
+            }
         }
     }
     printf("The sentence has %d words.\n", words);
